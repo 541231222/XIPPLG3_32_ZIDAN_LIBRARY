61bde502cf1ad97eda67344d48e1bb059e668daa
unknown <541231222@student.smk-telkom.pwt.sch.id>
2025-02-03 14:07:59 +0700

    crud for user

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\User2Controller;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -9,3 +10,5 @@ Route::get('/user', function (Request $request) {
 })->middleware('auth:sanctum');
 
 Route::apiResource('categories', CategoryController::class);
+
+Route::apiResource('users', User2Controller::class);
